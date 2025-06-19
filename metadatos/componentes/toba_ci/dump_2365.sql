0908446ec24bd136afbadc57c1fb004a5bbef366
Leonardo Fontes <lfontes@fca.uncu.edu.ar>
2025-06-19 11:43:29 -0300

    impresion jasper desde cuadro fichas OK

--- metadatos/componentes/toba_ci/dump_2365.sql
+++ metadatos/componentes/toba_ci/dump_2365.sql
@@ -198,40 +198,7 @@ INSERT INTO apex_objeto_eventos (proyecto, evento_id, objeto, identificador, eti
 	'0', --defecto
 	NULL, --display_datos_cargados
 	NULL, --grupo
-	'jasperreports', --accion
-	'0', --accion_imphtml_debug
-	NULL, --accion_vinculo_carpeta
-	NULL, --accion_vinculo_item
-	NULL, --accion_vinculo_objeto
-	'0', --accion_vinculo_popup
-	NULL, --accion_vinculo_popup_param
-	NULL, --accion_vinculo_target
-	NULL, --accion_vinculo_celda
-	NULL, --accion_vinculo_servicio
-	'0', --es_seleccion_multiple
-	'0'  --es_autovinculo
-);
-INSERT INTO apex_objeto_eventos (proyecto, evento_id, objeto, identificador, etiqueta, maneja_datos, sobre_fila, confirmacion, estilo, imagen_recurso_origen, imagen, en_botonera, ayuda, orden, ci_predep, implicito, defecto, display_datos_cargados, grupo, accion, accion_imphtml_debug, accion_vinculo_carpeta, accion_vinculo_item, accion_vinculo_objeto, accion_vinculo_popup, accion_vinculo_popup_param, accion_vinculo_target, accion_vinculo_celda, accion_vinculo_servicio, es_seleccion_multiple, es_autovinculo) VALUES (
-	'pruebas', --proyecto
-	'1339', --evento_id
-	'2365', --objeto
-	'imprimir', --identificador
-	'&Imprimir', --etiqueta
-	'0', --maneja_datos
-	NULL, --sobre_fila
-	NULL, --confirmacion
-	NULL, --estilo
-	'apex', --imagen_recurso_origen
-	'impresora.gif', --imagen
-	'1', --en_botonera
-	NULL, --ayuda
-	'6', --orden
-	NULL, --ci_predep
-	'0', --implicito
-	'0', --defecto
-	NULL, --display_datos_cargados
-	NULL, --grupo
-	'jasperreports', --accion
+	'H', --accion
 	'1', --accion_imphtml_debug
 	NULL, --accion_vinculo_carpeta
 	NULL, --accion_vinculo_item
@@ -426,12 +393,6 @@ INSERT INTO apex_eventos_pantalla (pantalla, objeto_ci, evento_id, proyecto) VAL
 	'pruebas'  --proyecto
 );
 INSERT INTO apex_eventos_pantalla (pantalla, objeto_ci, evento_id, proyecto) VALUES (
-	'1213', --pantalla
-	'2365', --objeto_ci
-	'1339', --evento_id
-	'pruebas'  --proyecto
-);
-INSERT INTO apex_eventos_pantalla (pantalla, objeto_ci, evento_id, proyecto) VALUES (
 	'1214', --pantalla
 	'2365', --objeto_ci
 	'1183', --evento_id
@@ -453,11 +414,5 @@ INSERT INTO apex_eventos_pantalla (pantalla, objeto_ci, evento_id, proyecto) VAL
 	'1214', --pantalla
 	'2365', --objeto_ci
 	'1308', --evento_id
-	'pruebas'  --proyecto
-);
-INSERT INTO apex_eventos_pantalla (pantalla, objeto_ci, evento_id, proyecto) VALUES (
-	'1214', --pantalla
-	'2365', --objeto_ci
-	'1339', --evento_id
 	'pruebas'  --proyecto
 );
