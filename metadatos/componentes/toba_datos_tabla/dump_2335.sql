d97cb29f2f25c14dda8e1834422a3e4a8cadac31
Leonardo Fontes <lfontes@fca.uncu.edu.ar>
2026-02-26 11:52:26 -0300

    correcion guarda tilde cargo_gestion

--- metadatos/componentes/toba_datos_tabla/dump_2335.sql
+++ metadatos/componentes/toba_datos_tabla/dump_2335.sql
@@ -226,4 +226,18 @@ INSERT INTO apex_objeto_db_registros_col (objeto_proyecto, objeto, col_id, colum
 	'0', --externa
 	NULL  --tabla
 );
+INSERT INTO apex_objeto_db_registros_col (objeto_proyecto, objeto, col_id, columna, tipo, pk, secuencia, largo, no_nulo, no_nulo_db, externa, tabla) VALUES (
+	'pruebas', --objeto_proyecto
+	'2335', --objeto
+	'1832000484', --col_id
+	'cargo_gestion', --columna
+	'L', --tipo
+	'0', --pk
+	NULL, --secuencia
+	NULL, --largo
+	NULL, --no_nulo
+	'0', --no_nulo_db
+	'0', --externa
+	NULL  --tabla
+);
 --- FIN Grupo de desarrollo 1832
